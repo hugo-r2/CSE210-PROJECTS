72cf88d5d69c8c8e7f7d5371ca01c764cac560ce
hugo-r2 <hugisr@gmail.com>
2024-09-25 17:40:49 -0600

    Prep 4 Exercise

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -999;
+        int total = 0;
+        int counter = 0;
+        string answer = "X";
+        int largest = -100;
+
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            answer = Console.ReadLine();
+            number = int.Parse(answer);
+            numbers.Add(number);
+
+        }
+
+        foreach (int newNumber in numbers)
+        {
+            int sum = newNumber + total;
+            total = sum;
+            counter++;
+
+            if (newNumber > largest)
+            {
+                largest = newNumber;
+            }
+
+        }
+
+        int average = total / (counter - 1);
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
