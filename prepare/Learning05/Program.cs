eedddd3ccbbcb73f640c30cb47b496f0349bb3fa
hugo-r2 <hugisr@gmail.com>
2024-10-30 00:54:32 -0600

    Assignments -Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        MathAssignment mathAssignment1 = new MathAssignment("Hugo Reyes", "Trigonometry", "Section 4.3", "Problems 45-78");
+        Console.WriteLine(mathAssignment1.GetHomeworkList());
+        Console.WriteLine(mathAssignment1.GetSummary());
+
+        WritingAssignment writingAssignment1 = new WritingAssignment("Mishell García", "School Organization", "How to propperly organize the Archive");
+        Console.WriteLine(writingAssignment1.GetWritingInformation());
+        Console.WriteLine(writingAssignment1.GetSummary());
     }
 }
